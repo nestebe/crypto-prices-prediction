79627bb4e2563a1ec95804a4242e92b1962e328c
Nicolas <nicolas.estebe@agentil.com>
2021-09-20 22:15:33 +0200

    save

---
+++ predict.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as ptl
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential
+
+
+crypto = 'ETH'
+currency = 'EUR'
+
+start = dt.datetime(2016,1,1)
+end = dt.datetime.now()
+
+data = web.DataReader(f'{crypto}-{currency}', 'yahoo', start, end)
+
+# Prepare Data
+print(data)
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train, y_train = [], []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0]) 
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# Neural network
+
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+#Test Model
